198fb78ccfd9442ec07a97ee37de9bb157fe4b6b
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-11-29 08:50:27 +0900

    各スクリーンを作成して、Homeを表示

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import Forgot from './screens/Forgot';
+import Drawer from './screens/Drawer';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Home />
+        );
+    }
+}
---
+++ screens/Forgot.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Forgot extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Forgot</Text>
+            </View>
+        );
+    }
+}
+
+export default Forgot;
