84277334316c8552c6512a994f393d95ae367634
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-11-29 10:41:06 +0900

    サインアップおよびリセットメール画面の簡易実装

--- screens/Forgot.js
+++ screens/Forgot.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+//react-native-elements
+import { Card, Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 class Forgot extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Forgot</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='パスワード忘れ'>
+                    <FormLabel>Email</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>errors</FormValidationMessage>
+                    <Button
+                        title='リセットメールを送信'
+                        onPress={() => this.handleForgot()}
+                        buttonStyle={{ marginTop: 20 }}
+                    />
+                </Card>
             </View>
         );
+    }
+
+    //リセットメール送信ボタンを押したとき
+    handleForgot = () => {
+        alert('リセットメールを送信しました。')
     }
 }
 
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+//react-native-elements
+import { Card, Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+
+//redux
+import { connect } from 'react-redux';
+import { updateUserData } from '../actions/userAction';
+
 
 class SignUp extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>SignUp</Text>
+            <View style={{ flex: 1, paddingVertical: 20 }}>
+                <Card title='サインアップ'>
+                    <FormLabel>名前</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>errors</FormValidationMessage>
+                    <FormLabel>Email（サインインIDとなります）</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>errors</FormValidationMessage>
+                    <FormLabel>パスワード</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>errors</FormValidationMessage>
+                    <FormLabel>パスワード（確認）</FormLabel>
+                    <FormInput />
+                    <FormValidationMessage>errors</FormValidationMessage>
+                    <Button
+                        title='サインアップ'
+                        onPress={() => this.handleSignUp()}
+                        buttonStyle={{ marginTop: 20 }}
+                        backgroundColor='#CC9933'
+                    />
+                </Card>
             </View>
         );
     }
+
+    handleSignUp = () => {
+        user = {
+            id: 77,
+            name: 'foo',
+            email: 'foo@hoge.com',
+            signedIn: true,
+        }
+        this.props.updateUserData(user);
+        this.props.navigation.navigate('SignedIn')
+    }
 }
 
-export default SignUp;
+//redux
+const mapStateToProps = state => (
+    {
+        state: state,
+    }
+);
+
+const mapDispatchToProps = dispatch => (
+    {
+        updateUserData: (user) => dispatch(updateUserData(user)),
+    }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
+// export default SignUp;
