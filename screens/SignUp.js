a351cdb41d2ea7668b83c3b8ae005b47b21f783d
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-11-29 12:28:40 +0900

    サインアップ画面にFormik, Yupを適用

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -7,43 +7,110 @@ import { Card, Button, FormLabel, FormInput, FormValidationMessage } from 'react
 import { connect } from 'react-redux';
 import { updateUserData } from '../actions/userAction';
 
+//formik
+import { Formik, yupToFormErrors } from 'formik';
+import * as Yup from 'yup';
 
 class SignUp extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, paddingVertical: 20 }}>
-                <Card title='サインアップ'>
-                    <FormLabel>名前</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>errors</FormValidationMessage>
-                    <FormLabel>Email（サインインIDとなります）</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>errors</FormValidationMessage>
-                    <FormLabel>パスワード</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>errors</FormValidationMessage>
-                    <FormLabel>パスワード（確認）</FormLabel>
-                    <FormInput />
-                    <FormValidationMessage>errors</FormValidationMessage>
-                    <Button
-                        title='サインアップ'
-                        onPress={() => this.handleSignUp()}
-                        buttonStyle={{ marginTop: 20 }}
-                        backgroundColor='#CC9933'
-                    />
-                </Card>
+                <Formik
+                    initialValues={{
+                        name: 'test',
+                        email: 'test@test.com',
+                        password: 'testtest',
+                        passwordConfirm: 'testtest',
+                    }}
+                    onSubmit={(values) => this.handleSignUp(values)}
+                    validationSchema={Yup.object().shape({
+                        name: Yup
+                            .string()
+                            .min(4, '名前は4文字以上8文字以内で入力して下さい。')
+                            .max(8, '名前は4文字以上8文字以内で入力して下さい。')
+                            .required('名前の入力は必須です。'),
+                        email: Yup
+                            .string()
+                            .email('Emailの形式ではないようです。')
+                            .required('Emailの入力は必須です。'),
+                        password: Yup
+                            .string()
+                            .min(4, '4文字以上で入力して下さい。')
+                            .required('パスワードの入力は必須です。'),
+                        passwordConfirm: Yup
+                            .string()
+                            .required('パスワードの確認は必須です。')
+                            .oneOf([Yup.ref('password')], 'パスワードが一致しません。'),
+                    })}
+                >
+                    {
+                        ({ handleSubmit, handleChange, values, errors, touched, handleBlur }) => (
+                            <Card title='サインアップ'>
+                                <FormLabel>名前</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.name}
+                                    onChangeText={handleChange('name')}
+                                    onBlur={handleBlur('name')}
+                                />
+                                {(touched.name && errors.name) && <FormValidationMessage>{errors.name}</FormValidationMessage>}
+                                <FormLabel>Email（サインインIDとなります）</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.email}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                />
+                                {(touched.email && errors.email) && <FormValidationMessage>{errors.email}</FormValidationMessage>}
+                                <FormLabel>パスワード</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.password}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    secureTextEntry
+                                />
+                                {(touched.password && errors.password) && <FormValidationMessage>{errors.password}</FormValidationMessage>}
+                                <FormLabel>パスワード（確認）</FormLabel>
+                                <FormInput
+                                    autoCapitalize='none'
+                                    value={values.passwordConfirm}
+                                    onChangeText={handleChange('passwordConfirm')}
+                                    onBlur={handleBlur('passwordConfirm')}
+                                    secureTextEntry
+                                />
+                                {(touched.passwordConfirm && errors.passwordConfirm) && <FormValidationMessage>{errors.passwordConfirm}</FormValidationMessage>}
+                                <Button
+                                    title='サインアップ'
+                                    onPress={handleSubmit}
+                                    buttonStyle={{ marginTop: 20 }}
+                                    backgroundColor='#CC9933'
+                                />
+                            </Card>
+                        )
+                    }
+                </Formik>
             </View>
         );
     }
 
-    handleSignUp = () => {
+    handleSignUp = (values) => {
+
+        //値の取得
+        const name = values.name;
+        const email = values.email;
+        const password = values.password;
+        const passwordConfirm = values.passwordConfirm;
+        //値の設定
         user = {
             id: 77,
-            name: 'foo',
-            email: 'foo@hoge.com',
+            name: name,
+            email: email,
             signedIn: true,
         }
+        //値の更新
         this.props.updateUserData(user);
+        //移動
         this.props.navigation.navigate('SignedIn')
     }
 }
